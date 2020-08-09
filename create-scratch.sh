8cf65b95859284085dcfcaef1e8f399b772ef8e2
Vit Mistina <vit.mistina@gmail.com>
2020-08-09 13:09:38 +0200

    permission set assignment

--- create-scratch.sh
+++ create-scratch.sh
@@ -2,4 +2,5 @@ sfdx force:org:create -f config/project-scratch-def.json -a Scratch -d 14 -s
 sfdx force:source:push
 sfdx force:user:create -a salesguy -f config/sales-user.json username=sales.user$(date +%s)@country-details.org
 sfdx force:user:create -a contractmgt -f config/contract-manager.json username=contract.manager$(date +%s)@country-details.org
+sfdx force:user:permset:assign -n CountryDetails -o salesguy,contractmgt
 sfdx force:apex:execute -f ./anonymous/restcountries-init.apex
