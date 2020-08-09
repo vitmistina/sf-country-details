64872df2dfb4cdcb0cf4628bcf32c3d023c0628a
Vit Mistina <vit.mistina@gmail.com>
2020-08-09 12:27:42 +0200

    launch scripts

--- create-scratch.sh
+++ create-scratch.sh
@@ -2,3 +2,4 @@ sfdx force:org:create -f config/project-scratch-def.json -a Scratch -d 14 -s
 sfdx force:source:push
 sfdx force:user:create -a salesguy -f config/sales-user.json username=sales.user$(date +%s)@country-details.org
 sfdx force:user:create -a contractmgt -f config/contract-manager.json username=contract.manager$(date +%s)@country-details.org
+sfdx force:apex:execute -f ./anonymous/restcountries-init.apex
